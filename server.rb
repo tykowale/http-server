e520309906dfa3e9f729472dcd241537153d46b9
Tyler Kowalewski <tykowale@gmail.com>
2015-03-07 10:10:11 -0600

    Something is wrong here and I can't figure it out

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'uri'
 
 hostname = 'localhost'
 port = 2000
-path = <<-HTML
+welcome = <<-HTML
 <html>
 <head>
   <title>Welcome</title>
@@ -16,22 +16,56 @@ path = <<-HTML
 </html>
 HTML
 
+profile = <<-HTML
+<html>
+<head>
+  <title>My Profile Page</title>
+</head>
+<body>
+  <p>This is my profile page.</p>
+</body>
+</html>
+HTML
+
+error = <<-HTML
+<html>
+<head>
+  <title>Not Found</title>
+</head>
+<body>
+  <p>This page was not found.</p>
+</body>
+</html>
+HTML
+
 def controller(request)
   request_line = request.split(' ')[1]
   path = URI(request_line).path
+  return path
 end
 
 server = TCPServer.new(port)
 loop {
   client = server.accept
   response = client.gets
-  client.print "HTTP/1.1 200 OK\r\n" +
+  if controller(response)[1..-1] == "welcome" || "profile"
+    client.print "HTTP/1.1 200 OK\r\n" +
                "Date: #{Time.now.ctime}\r\n" +
                "Server: #{`uname -sr`}" +
                "Content-Type: text/html; charset=UTF-8\r\n" +
-               "Content-Length: #{path.bytesize}\r\n" +
+               "Content-Length: #{welcome.bytesize}\r\n" +
                "Connection: close\r\n"
-  client.print "\r\n"
-  client.print path
+    client.print "\r\n"
+    client.print welcome
+  else
+    client.print "HTTP/1.1 404 NOT FOUND\r\n" +
+               "Date: #{Time.now.ctime}\r\n" +
+               "Server: #{`uname -sr`}" +
+               "Content-Type: text/html; charset=UTF-8\r\n" +
+               "Content-Length: #{error.bytesize}\r\n" +
+               "Connection: close\r\n"
+    client.print "\r\n"
+    client.print error
+  end
   client.close
 }
